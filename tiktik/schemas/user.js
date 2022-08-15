1013120f0571409c5ddb566ce664b3a7b2eefdcf
ValentinKrajzelman <valentinkrajzelman@hotmail.com>
2022-08-15 10:07:32 -0300

    asdf

---
+++ tiktik/schemas/user.js
@@ -0,0 +1,20 @@
+export default {
+    name: 'user',
+    title: 'User',
+    type: 'document',
+    fields: [
+        {
+            name: 'userName',
+            title: 'user Name',
+            type: 'string'
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'string'
+        }
+    ]
+}
+
+//nose si hay que poner una coma despues de type
+//fijate si te tira problemas
