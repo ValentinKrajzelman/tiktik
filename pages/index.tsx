5de7159bd7f9df8be7d494f782d1c914416cb769
ValentinKrajzelman <valentinkrajzelman@hotmail.com>
2022-09-06 18:39:46 -0300

    cosas

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import type { NextPage } from 'next'
+import axios from 'axios'
+
 
 const Home: NextPage = () => {
   return (
-    <div className='bg-green-400'>
+    <div className=''>
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+
 }
 
 export default Home
