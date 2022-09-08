d5d16bb1ea4d990df4c00406847238de4ea9c132
ValentinKrajzelman <valentinkrajzelman@hotmail.com>
2022-09-08 14:02:32 -0300

    cosas

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,11 @@ import type { NextPage } from 'next'
 import axios from 'axios'
 
 
-const Home: NextPage = () => {
-  return (
-    <div className=''>
+//just testing some things
+
+  t Home: NextPage = () => {
+  turn (
+  <div className=''>
     </div>
   )
 }
