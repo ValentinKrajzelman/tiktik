80228b4dae898972b74bede8400696e91175afb9
ValentinKrajzelman <valentinkrajzelman@hotmail.com>
2022-09-15 10:55:26 -0300

    cosas

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import '../styles/globals.css'
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
 
-//jklh
+
 
 const myApp = ({ Component, pageProps }: AppProps) => {
 
