ead00bbe8997d5d9f43de2d0757e773cce0075eb
ValentinKrajzelman <valentinkrajzelman@hotmail.com>
2022-09-20 14:53:56 -0300

    cosas

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import type { NextPage } from 'next'
 import axios from 'axios'
 
-
+//fijate este error
 
 
   t Home: NextPage = () => {
-  turn (
-  <div className=''>
+  turn(
+    <div className=''>
     </div>
   )
 }
